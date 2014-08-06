70359172c85bcf02354ecf8e7f3d3c289cb68ba2
PauloFagundes <paulo.esantos@gmail.com>
2014-08-06 17:46:43 -0300

    Inicio questão 6

---
+++ PythonParaZumbis/Lista1/questao6.py
@@ -0,0 +1,6 @@
+# Paulo Eduardo Faundes dos Santos
+# Curso Python para Zumbis
+# Lista de Exercício 1
+# Questão 6
+# Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer
+# e a velocidade média esperada para a viagem
