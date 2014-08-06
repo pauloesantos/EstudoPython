758414f82cfa974980d09f4c21a8540d9b5924a9
PauloFagundes <paulo.esantos@gmail.com>
2014-08-06 11:02:55 -0300

    Correção

--- PythonParaZumbis/Lista1/questao3.py
+++ PythonParaZumbis/Lista1/questao3.py
@@ -4,7 +4,8 @@
 # Questão 3
 # Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
 dias = int(input("Entre com os dias: "))
-horas = int(input("Entre com os as horas: "))
+horas = int(input("Entre com as horas: "))
 minutos = int(input("Entre com os minutos: "))
 segundos = int(input("Entre com os segundos: "))
-print(" %i dias, %i horas, %i minutos e %i segundos são %i segundos no total" % (dias, horas, minutos, segundos, (((dias * 24) + horas * 60) + minutos * 60) + segundos))
+total = ((dias * 24 * 60 * 60) + horas * 60 * 60) + minutos * 60) + segundos)
+print(" %i dias, %i horas, %i minutos e %i segundos são %i segundos no total" % (dias, horas, minutos, segundos, total))
